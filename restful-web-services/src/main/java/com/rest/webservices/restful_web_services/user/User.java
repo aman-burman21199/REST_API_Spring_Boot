c56c0fbe547118e07d63400e85fc68186d126b32
Aman Burman <amanb21199@gmail.com>
2025-03-24 18:06:28 +0530

    Post Entity with ManyToOne relationship with User

--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/User.java
@@ -1,12 +1,15 @@
 package com.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -28,6 +31,11 @@ public class User {
 	
 	@Past(message = "Birth Dte should be in the past.")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> posts;
+	
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
 		this.id = id;
--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into user_details(id,birth_date,name)
 values(100,current_date(),'Aman');
+
+insert into post(id,description,user_id)
+values(1,'Learn ML and AI',100);
+
+insert into post(id,description,user_id)
+values(2,'Learn Java Spring Boot',100);
